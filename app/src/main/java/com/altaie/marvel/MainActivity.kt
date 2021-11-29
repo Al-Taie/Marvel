afedfc542ca16eaba57db531564c32d1d63113ac
Al-Taie <agprosup@gmail.com>
2021-11-29 19:06:19 +0300

    Initial commit

---
+++ app/src/main/java/com/altaie/marvel/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.altaie.marvel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.altaie.marvel.utils.toMD5
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
