f37657c1c75fdc19a96c9004ed97bae6799751b0
Al-Taie <agprosup@gmail.com>
2021-12-01 20:05:35 +0300

    fix: create character item ui

---
+++ app/src/main/java/com/altaie/marvel/ui/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.altaie.marvel.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.altaie.marvel.databinding.FragmentHomeBinding
+import com.altaie.marvel.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override val viewModel: HomeViewModel by activityViewModels()
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentHomeBinding
+        get() = FragmentHomeBinding::inflate
+}
---
+++ app/src/main/java/com/altaie/marvel/ui/home/HomeInteractionListener.kt
@@ -0,0 +1,7 @@
+package com.altaie.marvel.ui.home
+
+import com.altaie.marvel.ui.base.BaseInteractionListener
+
+interface HomeInteractionListener : BaseInteractionListener {
+    fun onRecyclerItemClicked()
+}
