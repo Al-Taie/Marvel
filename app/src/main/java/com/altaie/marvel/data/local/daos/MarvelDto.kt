6f2a48a5ed0e93b518e3d2a93c6f19010ac1cbfa
Al-Taie <agprosup@gmail.com>
2021-12-04 17:17:37 +0300

    fix: add more functions to dao

--- app/src/main/java/com/altaie/marvel/data/local/daos/MarvelDto.kt
+++ app/src/main/java/com/altaie/marvel/data/local/daos/MarvelDto.kt
@@ -4,8 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.altaie.marvel.data.local.entities.CharacterEntity
-import com.altaie.marvel.data.local.entities.ComicEntity
+import com.altaie.marvel.data.local.entities.*
 
 @Dao
 interface MarvelDto {
@@ -15,9 +14,33 @@ interface MarvelDto {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertComics(characters: List<ComicEntity>)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCreators(creators: List<CreatorEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEvents(events: List<EventEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSeries(series: List<SeriesEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStories(stories: List<StoryEntity>)
+
     @Query("SELECT * FROM TB_CHARACTERS")
     suspend fun getAllCharacters() : List<CharacterEntity>
 
     @Query("SELECT * FROM TB_COMICS")
     suspend fun getAllComics() : List<ComicEntity>
+
+    @Query("SELECT * FROM TB_CREATORS")
+    suspend fun getAllCreators() : List<CreatorEntity>
+
+    @Query("SELECT * FROM TB_EVENTS")
+    suspend fun getAllEvents() : List<EventEntity>
+
+    @Query("SELECT * FROM TB_SERIES")
+    suspend fun getAllSeries() : List<SeriesEntity>
+
+    @Query("SELECT * FROM TB_STORIES")
+    suspend fun getAllStories() : List<StoryEntity>
 }
