df2ba3b3f88480f245bb181c65b516c55c937da2
Al-Taie <agprosup@gmail.com>
2021-12-06 13:49:59 +0300

    fix: add safe list cast extension function

--- app/src/main/java/com/altaie/marvel/utils/extensions.kt
+++ app/src/main/java/com/altaie/marvel/utils/extensions.kt
@@ -4,3 +4,7 @@ import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils.md5
 
 fun String.toMD5(): String = Hex.encodeHexString(md5(this))
+
+inline fun <T, reified R> List<T>.castTo(): List<R> {
+    return this.filterIsInstance<R>().takeIf { it.size == this.size } ?: emptyList<R>()
+}
